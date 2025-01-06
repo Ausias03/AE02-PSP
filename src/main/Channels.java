9322e56bde529feaa3c4f8c812457728a06b75a2
rusafe <150953396+rusafe@users.noreply.github.com>
2025-01-06 13:45:55 +0100

    Created channels class

---
+++ src/main/Channels.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Channels {
+	private static final String fileLocation = "resources/channels.txt";
+	
+	public static String getChannels() {
+		try (BufferedReader br = new BufferedReader(new FileReader(fileLocation))) {
+			String channels = "[";
+			String line;
+			int i = 1;
+			while((line = br.readLine()) != null) {
+				if(i != 1)
+					channels += ", ";
+				channels += String.format("%d-%s", i, line);
+				i++;
+			}
+			channels += "]";
+			return channels;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+}
