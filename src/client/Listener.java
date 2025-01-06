4c8930e6d0d5abc39b95a4a703f5fcb9a3ba819e
Ausias <150703040+Ausias03@users.noreply.github.com>
2025-01-06 18:49:05 +0100

    Added logic to Listener class

--- src/client/Listener.java
+++ src/client/Listener.java
@@ -1,23 +1,26 @@
 package client;
 
 import java.io.BufferedReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class Listener implements Runnable{
+public class Listener implements Runnable {
 	private BufferedReader br;
 
-    public Listener(BufferedReader br) {
-        this.br = br;
-    }
+	public Listener(BufferedReader br) {
+		this.br = br;
+	}
 
-    @Override
-    public void run() {
-        try {
-            String message;
-            while ((message = br.readLine()) != null) {
-                System.out.println("Servidor: " + message);
-            }
-        } catch (Exception e) {
-            System.out.println("Conexión cerrada por el servidor.");
-        }
-    }
+	@Override
+	public void run() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("mm/dd-hh:mm:ss");
+		try {
+			String message;
+			while ((message = br.readLine()) != null) {
+				System.out.println(dateFormat.format(new Date()) + ": " + message);
+			}
+		} catch (Exception e) {
+			System.out.println("Connection closed.");
+		}
+	}
 }
